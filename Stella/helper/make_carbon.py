43d7ed197224a38b2397f10aaccdf567c83d7971
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-11-23 12:32:12 +0530

    Create make_carbon.py

---
+++ Stella/helper/make_carbon.py
@@ -0,0 +1,8 @@
+import asyncio
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiosession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
