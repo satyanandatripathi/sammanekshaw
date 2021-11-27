9d6420c91dd6f48a9a5561953d03c0a0d190c28d
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-11-27 11:38:46 +0530

    Create orgasm.py

---
+++ Stella/helper/orgasm.py
@@ -0,0 +1,38 @@
+import asyncio
+from pyrogram.types import Message
+from typing import Union
+import re
+import os
+from datetime import datetime
+from typing import List
+
+
+#DoNotAsync
+
+async def extract_user(message: Message) -> Union[int, str]:
+    """extracts the user from a message"""
+    user_id = None
+    user_first_name = None
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        user_first_name = message.reply_to_message.from_user.first_name
+
+
+#DoNotAsync
+async def last_online(from_user):
+    time = ""
+    if from_user.is_bot:
+        time += "🤖 Bot :("
+    elif from_user.status == 'recently':
+        time += "Recently"
+    elif from_user.status == 'within_week':
+        time += "Within the last week"
+    elif from_user.status == 'within_month':
+        time += "Within the last month"
+    elif from_user.status == 'long_time_ago':
+        time += "A long time ago :("
+    elif from_user.status == 'online':
+        time += "Currently Online"
+    elif from_user.status == 'offline':
+        time += datetime.fromtimestamp(from_user.last_online_date).strftime("%a, %d %b %Y, %H:%M:%S")
+    return time
