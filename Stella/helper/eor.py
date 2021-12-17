cb429e1a9d8179dab3e4583b5b05db648a750a68
Vɪɴᴀʏᴀᴋ Pᴀɴᴅᴇʏ <87496159+Harpia-Vieillot@users.noreply.github.com>
2021-12-17 20:06:56 +0530

    Create eor.py

---
+++ Stella/helper/eor.py
@@ -0,0 +1,9 @@
+from inspect import getfullargspec
+async def eor(msg: Message, **kwargs):
+    func = (
+        (msg.edit_text if msg.from_user.is_self else msg.reply)
+        if msg.from_user
+        else msg.reply
+    )
+    spec = getfullargspec(func.__wrapped__).args
+    return await func(**{k: v for k, v in kwargs.items() if k in spec})
