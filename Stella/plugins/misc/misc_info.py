f2b6ee2fa28bb0f49fb69d94adc7f9cf63f45b12
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-12-24 11:06:50 +0530

    Huh

---
+++ Stella/plugins/misc/misc_info.py
@@ -0,0 +1,10 @@
+__mod_name__ = "Misc"
+
+__help__ = (
+    "Extra helpful functions of Sam Manekshaw :)\n\n"
+    " - /paste : to paste all text of replied file\n"
+    " - /ud `<word>` : to search given word in urban dictionary\n"
+    " - /info `<reply to user | username | user id>` : to fetch information about given entity\n"
+    " - /id `<reply to user | chat id>` : to fetch Telegram id of user / chat\n"
+    " - /carbon `<reply to message>` : carbonise the given text
+)   
