34414b53268ad46968ba9cf47652bc4309ea14f0
Vɪɴᴀʏᴀᴋ Pᴀɴᴅᴇʏ <87496159+Harpia-Vieillot@users.noreply.github.com>
2021-12-25 03:24:41 +0530

    Create privacy_data.py

---
+++ Stella/plugins/privacy/privacy_data.py
@@ -0,0 +1,5 @@
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from Stella import StellaCli
+from Stella.helper import custom_filter
+
+## WILL WORK ON THIS SHIT LATER
