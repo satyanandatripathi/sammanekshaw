6866bac129636ef0f5e2ebafb78ea7dd0c13d8a8
Vɪɴᴀʏᴀᴋ Pᴀɴᴅᴇʏ <87496159+Harpia-Vieillot@users.noreply.github.com>
2021-12-24 22:33:03 +0530

    Create privacy_policy.py

---
+++ Stella/plugins/privacy/privacy_policy.py
@@ -0,0 +1,5 @@
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from Stella import StellaCli
+from Stella.helper import custom_filter
+
+## will work on this later 
