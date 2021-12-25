ef6d0b58cb2116f53d7fd2b9911240872227c8d4
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-12-25 17:56:38 +0530

    Ishhh

--- Stella/plugins/privacy/privacy.py
+++ Stella/plugins/privacy/privacy.py
@@ -1,6 +1,7 @@
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from Stella import StellaCli
 from Stella.helper import custom_filter
+from pyrogram import filters
 
 @StellaCli.on_message(custom_filter.command(commands=('privacy')))
 async def urbanDictionary(client, message):
